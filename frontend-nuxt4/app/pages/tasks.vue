858ed243b8771bff9d3ea5fa7f6e91b099a13cca
DazB <admin@mycontenthub.app>
2025-09-30 08:11:12 +1000

    Fix v-else error in tasks.vue - remove duplicate empty state

--- frontend-nuxt4/app/pages/tasks.vue
+++ frontend-nuxt4/app/pages/tasks.vue
@@ -280,17 +280,6 @@
                   </div>
                 </div>
               </div>
-
-              <!-- Empty State -->
-              <div v-else class="text-center py-8">
-                <div class="w-16 h-16 mx-auto mb-4 bg-orange-500/10 rounded-full flex items-center justify-center">
-                  <svg class="h-8 w-8 text-orange-400" fill="currentColor" viewBox="0 0 20 20">
-                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/>
-                  </svg>
-                </div>
-                <h4 class="text-white font-medium mb-2">All tasks scheduled!</h4>
-                <p class="text-sm text-gray-400">Great job! All your tasks have due dates assigned.</p>
-              </div>
             </div>
 
 
